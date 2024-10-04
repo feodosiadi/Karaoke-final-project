f7cd72d175d84be75dcc07f0127c2c71d5cec6a0
feodosiadi <feodosiadi@gmail.com>
2024-10-04 14:55:13 +0300

    123

--- server/src/routes/genres.router.js
+++ server/src/routes/genres.router.js
@@ -1,3 +1,17 @@
+const genreService = require('../services/genreService');
+
 const genresRouter = require('express').Router();
+
+genresRouter.route('/').get(async (req, res) => {
+  try {
+    const allGenres = await genreService.getAllGenres()    
+    return res.status(200).json(allGenres) 
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ text: 'allgenres problem server', message: error.message });
+  }
+});
 
 module.exports = genresRouter;
