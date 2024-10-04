2920118648c86faa8d91beded7da1d0f35244b78
Сергей Антипов <sergejantipov@MacBook-Air-Sergej.local>
2024-10-04 12:20:05 +0300

    Cloud db

--- server/db/database.js
+++ server/db/database.js
@@ -1,11 +1,10 @@
-require('dotenv').config();
-
 module.exports = {
   development: {
+    // use_env_variable: process.env.DATABASE_URL,
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    host: process.env.HOST,
+    host: process.env.DB_HOST,
     dialect: 'postgres',
     seederStorage: 'sequelize',
     seederStorageTableName: 'SequelizeData',
