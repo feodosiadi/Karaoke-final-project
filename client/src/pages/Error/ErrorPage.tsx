9ceb02095dc1441ade3fc13927617254b5d23ab9
Сергей Антипов <sergejantipov@MacBook-Air-Sergej.local>
2024-10-08 17:12:47 +0300

    Error Page

--- client/src/pages/Error/ErrorPage.tsx
+++ client/src/pages/Error/ErrorPage.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import styles from './ErrorPage.module.css';
 
-export default function ErrorPage():JSX.Element {
+export default function ErrorPage(): JSX.Element {
+  const [showText, setShowText] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowText(true);
+    }, 2000); 
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div>
-      ERORE
+    <div className={styles.errorContainer}>
+      <img src="../../../public/errorPage/anton.png" alt="Error" className={styles.animatedImage} />
+      {showText && <div className={styles.errorText}>ОЛОЛО НЕ ПРОЙДЕТ!!!</div>}
     </div>
-  )
+  );
 }
